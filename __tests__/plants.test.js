f50964a81fce0c508b3bd4c3200c5ab086e275ba
cass1618 <cass16180@gmail.com>
2021-06-22 13:22:21 -0700

    Add test for Plant constructor, pass
    Co-Authored-By: Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
    Co-Authored-by: Michael Burton <micbur1@gmail.com>

--- __tests__/plants.test.js
+++ __tests__/plants.test.js
@@ -7,3 +7,21 @@ describe('Plant', () => {
     expect(myPlant()).toMatchObject({ water: 2, soil: 4, light: 3 });
   });
 });
+
+// describe('Character', () => {
+//   test('should make an instance of new character', () => {
+//     const initialState = { brawn: 2, brains: 4, beauty: 3 }
+//     const myCharacter = tools.storeState(initialState);
+//     expect(myCharacter()).toMatchObject({ brawn: 2, brains: 4, beauty: 3 });
+//   });
+
+
+
+// describe('Plant', () => {
+//   test('should make an instance of new plant', () => {
+//     // const initialState = { water: 2, soil: 4, light: 3 }
+//     // const myPlant = plants.storeState(initialState);
+//     let myPlant = new Plant();
+//     expect(myPlant).toMatchObject({ water: 0, soil: 0, light: 0 });
+//   });
+// });
--- src/js/plants.js
+++ src/js/plants.js
@@ -1,3 +1,51 @@
 export const plants = () => {
-    return 0;
+  return 0;
 }
+
+// export const hydrate = (plant) => {
+//   return {
+//     ...plant,
+//     water: (plant.water || 0) + 1
+//   }
+// };
+
+
+export const storeState = (initialState = {}) => {
+  let currentState = initialState;
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = { ...newState };
+    return newState;
+  }
+}
+
+// describe('Character', () => {
+//     test('should make an instance of new character', () => {
+//       const initialState = {brawn: 2, brains: 4, beauty:3}
+
+
+
+
+
+
+
+
+// export default class Plant {
+//   constructor() {
+//     this.water = 0;
+//     this.soil = 0;
+//     this.light = 0;
+//   }
+
+//   hydrate() {
+//     this.water++
+//   }
+
+//   feed() {
+//     this.soil++
+//   }
+
+//   giveLight() {
+//     this.light++
+//   }
+// }
